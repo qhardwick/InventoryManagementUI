212cca758d8f90c42c743f48e929e65d42b2d32d
Tyler <84280214+tylercausey@users.noreply.github.com>
2024-09-25 15:58:43 -0500

    added test for pipeline again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home-container">
-        <h1>Hello</h1>
+        <h1>Howdy</h1>
       <div className="landing-button">
         <Link className="landing-link" to={'/warehouses'}>Warehouse Manager</Link>
       </div>
